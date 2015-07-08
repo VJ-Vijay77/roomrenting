60f5824bef01fcf7442092eb9a226a959c488428
VJ-Vijay77 <vijaydinesh77vj@gmail.com>
2015-07-07 20:00:00 -0600

    changes

--- pkg/admincontroller/coupons.go
+++ pkg/admincontroller/coupons.go
@@ -86,6 +86,9 @@ func InactiveCoupons(c *gin.Context){
 }
 
 func ActiveCoupons(c *gin.Context){
+	//setting coupons
+	
+	//getting Id form the params
 	ID := c.Param("ID")
 	cid, _ := strconv.Atoi(ID)
 	
