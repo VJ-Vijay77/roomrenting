af6a83cf77f6445814e4a722eddc447c9d222112
Vijay Dinesh <vijaydinesh77vj@gmail.com>
2022-06-02 14:12:59 +0530

    block delete unblock  set

--- pkg/controller/updatedata.go
+++ pkg/controller/updatedata.go
@@ -6,8 +6,9 @@ import (
 	"github.com/gin-gonic/gin"
 	"tawesoft.co.uk/go/dialog"
 )
-
+//updating first name of user
 func UpdateFUser(c *gin.Context) {
+	
 	FName := c.Request.FormValue("updateuserdata")
 	if FName==""{
 		dialog.Alert("Enter data!!")
@@ -21,6 +22,8 @@ func UpdateFUser(c *gin.Context) {
 	c.Redirect(303, "/admin/user_management")
 
 }
+
+//updating last name of user
 func UpdateLUser(c *gin.Context) {
 
 	LName := c.Request.FormValue("updateuserdata")
@@ -29,11 +32,53 @@ func UpdateLUser(c *gin.Context) {
 		c.Redirect(303,"/admin/user_management")
 		return
 	}
-	
+
 	var data models.Users
 	ID := c.Param("ID")
 	db := database.GetDb()
 	db.Raw("UPDATE users SET last_name=? WHERE id=?", LName, ID).Scan(&data)
 	c.Redirect(303, "/admin/user_management")
 
+}
+
+//setting block status of user to false
+func BlockUser(c *gin.Context) {
+	var user models.Users
+	ID := c.Param("ID")
+	db := database.GetDb()
+	db.Raw("UPDATE users SET block_status=false WHERE id=?",ID).Scan(&user)
+	c.Redirect(303,"/admin/user_management")
+}
+
+//unblocking user
+func UnBlockUser(c *gin.Context) {
+	var user models.Users
+
+	ID := c.Param("ID")
+	db := database.GetDb()
+	db.Raw("SELECT block_status FROM users WHERE id=?",ID).Scan(&user)
+	if user.Block_Status{
+		dialog.Alert("Already in active!")
+		c.Redirect(303,"/admin/user_management")
+		return
+	}
+
+	db.Raw("UPDATE users SET block_status=true WHERE id=?",ID).Scan(&user)
+	c.Redirect(303,"/admin/user_management")
+	
+}
+
+//deleting user from database
+func DeleteUser(c *gin.Context) {
+	var user models.Users
+	ID := c.Param("ID")
+	if ID == "1"{
+		dialog.Alert("Could not delete, User has Admin privilages!")
+		c.Redirect(303,"/admin/user_management")
+		return
+	}
+	db := database.GetDb()
+	db.Raw("DELETE FROM users WHERE id=?",ID).Scan(&user)
+	c.Redirect(303,"/admin/user_management")
+	
 }
