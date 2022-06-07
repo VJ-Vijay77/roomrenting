2ea23397295a63ec3ea132485440ade38bf6466e
Vijay Dinesh <vijaydinesh77vj@gmail.com>
2022-06-07 12:15:21 +0530

    minor fixes

--- pkg/controller/searchuser.go
+++ pkg/controller/searchuser.go
@@ -10,25 +10,21 @@ import (
 //search user
 func SearchUsers(c *gin.Context) {
 	Name := c.PostForm("search")
-	// var result string
+	var result string
 	var results []models.Users
 	db := database.GetDb()
-	db.Raw("SELECT id,first_name,last_name,email,mobile FROM users WHERE first_name=?", Name).Scan(&results)
-	// if result == Name && result != ""{
-	// 	dialog.Alert("User Found")
-	// }else{
-	// dialog.Alert("User not found")
-	// }
-	for _,result := range results{
-	if result.First_Name != Name || result.First_Name == ""{
-		dialog.Alert("User Not found !!")
-		c.Redirect(303,"/admin/user_management/addusers")
-		return
-	}
-}
 
-	c.HTML(200,"addusers.gohtml",gin.H{
-		"userdata":results,
+	db.Raw("SELECT first_name FROM users WHERE first_name=?", Name).Scan(&result)
+
+	if result != Name || result == "" {
+		dialog.Alert("User Not Found")
+		c.Redirect(303, "/admin/user_management/addusers")
+		return
+	} 
+	db.Raw("SELECT id,first_name,last_name,email,mobile FROM users WHERE first_name=?", Name).Scan(&results)
+
+	c.HTML(200, "addusers.gohtml", gin.H{
+		"userdata": results,
 	})
 	//  c.Redirect(303, "/admin/user_management/addusers")
 	// c.HTML(200,"addusers.gohtml",gin.H{"data":result,})
