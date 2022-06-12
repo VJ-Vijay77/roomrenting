51cd76193f6ceac072cee1721b0270b27c1afb3d
Vijay Dinesh <vijaydinesh77vj@gmail.com>
2022-06-12 16:11:06 +0530

    updation on email done..session clear

--- pkg/controller/editprofile.go
+++ pkg/controller/editprofile.go
@@ -1,9 +1,12 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/VJ-Vijay77/r4room/pkg/database"
 	"github.com/VJ-Vijay77/r4room/pkg/models"
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/sessions"
 	"tawesoft.co.uk/go/dialog"
 )
 
@@ -19,14 +22,53 @@ func EditProfileFirstName(c *gin.Context) {
 }
 
 func EditProfileLastName(c *gin.Context) {
+	db := database.GetDb()
+
+	lastname := c.PostForm("lastnamef")
+	userID := c.Param("ID")
+	var lastnameupdation models.Users
+	db.Raw("UPDATE users SET last_name=? WHERE id=?", lastname, userID).Scan(&lastnameupdation)
+	dialog.Alert("Last name updated Successfully.")
+	c.Redirect(303, "/user/edit_profile")
 
 }
 
 func EditProfileMobile(c *gin.Context) {
+	db := database.GetDb()
 
+	mobile := c.PostForm("mobilef")
+	userID := c.Param("ID")
+	var mobileupdation models.Users
+	db.Raw("UPDATE users SET mobile=? WHERE id=?", mobile, userID).Scan(&mobileupdation)
+	dialog.Alert("Mobile number updated Successfully.")
+	c.Redirect(303, "/user/edit_profile")
 }
 
 func EditProfileEmail(c *gin.Context) {
+	db := database.GetDb()
+
+	session, _ := Store.Get(c.Request, "session")
+	oldemail := fmt.Sprintf("%v", session.Values["userID"])
+
+	email := c.PostForm("emailf")
+	userID := c.Param("ID")
+	if oldemail == email {
+		dialog.Alert("You entered same email! Try with different one!")
+		c.Redirect(303, "/user/edit_profile")
+		return
+	}
+
+	var emailupdation models.Users
+	db.Raw("UPDATE users SET email=? WHERE id=?", email, userID).Scan(&emailupdation)
+
+	dialog.Alert("Email updated Successfully.\n\t\tNOTE!!\nYour login email will be now %s\nYou may need to login again now!", email)
+	session.Options = &sessions.Options{
+		Path:     "/",
+		MaxAge:   -1,
+		HttpOnly: true,
+	}
+	session.Save(c.Request, c.Writer)
+	c.Redirect(303, "/user_login")
 
 }
 
