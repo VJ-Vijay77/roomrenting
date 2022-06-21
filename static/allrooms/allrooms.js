cba123a759aaaa5d2c9e62edc3f8c4bdc2c04a94
Vijay Dinesh <vijaydinesh77vj@gmail.com>
2022-06-22 01:27:13 +0530

    range button done for 3 section

--- static/allrooms/allrooms.js
+++ static/allrooms/allrooms.js
@@ -3,3 +3,58 @@ function isavailable(){
     
   location.href = "/user/all_room_search/filter/"+startdate
 }
+
+function range (){
+  let range = document.getElementById("customRange3").value
+  
+  if (range == 1){
+    document.getElementById("rangeid").innerHTML="Price Range : Above 5k"
+    location.href='/user/filter/abovefive';
+  }else if (range == 0){
+    document.getElementById("rangeid").innerHTML="Price Range : Below 5k"
+    location.href='/user/filter/belowfive';
+    
+  }else if ( range == 2 ){
+    document.getElementById("rangeid").innerHTML="Price Range : Above 7k"
+    location.href='/user/filter/aboveseven';
+
+  }
+}
+
+  function avrange (){
+    let range = document.getElementById("customRange3").value
+    
+    if (range == 1){
+      document.getElementById("rangeid").innerHTML="Price Range : Above 5k"
+      location.href='/user/filter/avabovefive';
+    }else if (range == 0){
+      document.getElementById("rangeid").innerHTML="Price Range : Below 5k"
+      location.href='/user/filter/avbelowfive';
+      
+    }else if ( range == 2 ){
+      document.getElementById("rangeid").innerHTML="Price Range : Above 7k"
+      location.href='/user/filter/avaboveseven';
+  
+    }
+
+
+}
+
+function bkrange (){
+  let range = document.getElementById("customRange3").value
+  
+  if (range == 1){
+    document.getElementById("rangeid").innerHTML="Price Range : Above 5k"
+    location.href='/user/filter/bkabovefive';
+  }else if (range == 0){
+    document.getElementById("rangeid").innerHTML="Price Range : Below 5k"
+    location.href='/user/filter/bkbelowfive';
+    
+  }else if ( range == 2 ){
+    document.getElementById("rangeid").innerHTML="Price Range : Above 7k"
+    location.href='/user/filter/bkaboveseven';
+
+  }
+
+
+}
